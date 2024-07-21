f4010e845ac254f630451c13f9a71ddc029fc434
Sergejs Kuzema <s.kuzema@gmail.com>
2024-07-21 20:41:49 +0300

    feat: lambda

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,8 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 
 export class CartStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const cartServiceLambda = new lambda.Function(this, 'CartServiceLambda', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: 'cartService.handler',
+      code: lambda.Code.fromAsset(
+        'C:/Users/skuze/OneDrive/Документы/Sergej/Kursi/AWS Cloud Developer/Task8_card/nodejs-aws-cart-api/lambda',
+      ),
+    });
+
+    new LambdaRestApi(this, 'CartServiceApi', {
+      handler: cartServiceLambda,
+    });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Handler, Context } from 'aws-lambda';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as express from 'express';
-import serverlessExpress from '@vendia/serverless-express';
 
-let serverlessExpressInstance: any;
+const expressApp = express();
 
-async function bootstrapServer() {
-  const expressApp = express();
-  const nestApp = await NestFactory.create(
+async function bootstrap() {
+  const app = await NestFactory.create(
     AppModule,
     new ExpressAdapter(expressApp),
   );
-  await nestApp.init();
-  return serverlessExpress({ app: expressApp });
+  await app.init();
 }
 
-export const handler: Handler = async (event: any, context: Context) => {
-  if (!serverlessExpressInstance) {
-    serverlessExpressInstance = await bootstrapServer();
-  }
-  return serverlessExpressInstance(event, context);
-};
+bootstrap();
